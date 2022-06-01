ce247a6afd5b1722f1e9c878e22ed1dda980cae2
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-05-31 19:33:02 -0600

    formatting howls

--- public/js/logout.js
+++ public/js/logout.js
@@ -14,3 +14,16 @@ const logout = async () => {
 document.querySelector('#logout').addEventListener('click', logout);
 
 
+const catFact = () => {
+  fetch('https://meowfacts.herokuapp.com')
+    .then(data => {
+      return data.json();
+    })
+    .then(post => {
+      //write to populate data into div
+      
+      console.log(post.data.toString());
+    })
+};
+
+catFact(); 
